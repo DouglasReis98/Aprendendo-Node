95c83ec886c5da221c61c12059ea6528af38ded4
DouglasReis <douglasamaralreis@outlook.com>
2024-02-09 21:23:06 -0300

    updates

--- node_mysql/index.js
+++ node_mysql/index.js
@@ -4,6 +4,14 @@ const mysql = require('mysql')
 
 const app = express()
 
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+
+app.use(express.json())
+
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars')
 
@@ -11,6 +19,41 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.render('home');
+})
+
+app.post('/books/insertbook', (req, res) => {
+    const title = req.body.title
+    const pageqty = req.body.pageqty
+
+    const sql = `INSERT INTO books (title, pageqty) VALUES ('${title}', '${pageqty}')`;
+
+    conn.query(sql, function(err) {
+        if (err) {
+            console.log(err)
+            return
+        }
+
+        res.redirect('/books')
+    })
+})
+
+app.get('/books', (req, res) => {
+    const sql = "SELECT * FROM books"
+
+    conn.query(sql, function(err, data) {
+        
+        if (err) {
+            console.log(err)
+            return
+        }
+
+        const books = data
+
+        console.log(books)
+
+        res.render('books', { books })
+
+    })
 })
 
 const conn = mysql.createConnection({
