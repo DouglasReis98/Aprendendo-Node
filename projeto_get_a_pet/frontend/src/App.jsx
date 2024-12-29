1074b520f9b981fbb29349e2567969e6596512fb
DouglasReis98 <y*********@gmail.com>
2024-12-29 19:22:01 -0300

    css_e_autenticacao

---
+++ projeto_get_a_pet/frontend/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+/* Components */
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Container from "./components/layout/Container";
+
+/* Pages */
+import Login from "./components/pages/Auth/Login";
+import Register from "./components/pages/Auth/Register";
+import Home from "./components/pages/Home";
+
+/* Context */
+import { UserProvider } from "./context/UserContext";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <UserProvider>
+        <Navbar />
+        <Container>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Container>
+        <Footer />
+      </UserProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
