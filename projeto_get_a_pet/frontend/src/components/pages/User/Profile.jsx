720b85ee58cbc16de6acd9ccaa9eb74d6608b18d
DouglasReis98 <y*********@gmail.com>
2025-01-20 21:20:07 -0300

    erro_foto_usuario

--- projeto_get_a_pet/frontend/src/components/pages/User/Profile.jsx
+++ projeto_get_a_pet/frontend/src/components/pages/User/Profile.jsx
@@ -4,33 +4,83 @@ import { useState, useEffect } from "react";
 
 import styles from "./Profile.module.css";
 import formStyles from "../../form/Form.module.css";
+
 import Input from "../../form/Input";
+
+import useFlashMessage from "../../../hooks/useFlashMessages";
 
 function Profile() {
   const [user, setUser] = useState({});
-  const [token] = useState(localStorage.getItem("token") || '');
+  const [preview, setPreview] = useState()
+  const [token] = useState(localStorage.getItem("token") || "");
+  const { setFlashMessage } = useFlashMessage();
 
   useEffect(() => {
-    api.get('/users/checkuser', {
+    api
+      .get("/users/checkuser", {
         headers: {
-            Authorization: `Bearer ${JSON.parse(token)}`
-        }
-    }).then((response) => {
-        setUser(response.data)
-    })
-  }, [token])
-  function onFileChange(e) {}
+          Authorization: `Bearer ${JSON.parse(token)}`,
+        },
+      })
+      .then((response) => {
+        setUser(response.data);
+      });
+  }, [token]);
+  function onFileChange(e) {
+    setPreview(e.target.files[0])
+    setUser({ ...user, [e.target.name]: e.target.files[0] });
+  }
 
-  function handleChange(e) {}
+  function handleChange(e) {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    let msgType = "success";
+
+    const formData = new FormData();
+
+    await Object.keys(user).forEach((key) => formData.append(key, user[key]));
+
+    const data = await api
+      .patch(`/users/edit/${user._id}`, formData, {
+        headers: {
+          Authorization: `Bearer ${JSON.parse(token)}`,
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((response) => {
+        return response.data;
+      })
+      .catch((err) => {
+        msgType = "error";
+        return err.response.data;
+      });
+
+    setFlashMessage(data.message, msgType);
+  }
 
   return (
     <section>
       <div className={styles.profile_header}>
         <h1>Perfil</h1>
-        <p>Preview Imagem</p>
+        <p>
+          {(user.image || preview) && (
+            <img
+              src={
+                preview
+                  ? URL.createObjectURL(preview)
+                  : `${process.env.REACT_APP_API}/images/users/${user.image}`
+              }
+              alt={user.name}
+            />
+          )}
+        </p>
       </div>
 
-      <form className={formStyles.form_container}>
+      <form onSubmit={handleSubmit} className={formStyles.form_container}>
         <Input
           text="Imagem"
           type="file"
